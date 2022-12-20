91c5f47ca0e04ac01b3f1a08b15b7e5f3613ffdf
Ivan Lesar <ivan.lesar.pmf@gmail.com>
2022-12-20 20:20:05 +0100

    add day 17

--- day17/task1.js
+++ day17/task1.js
@@ -1,198 +1,246 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const input = fs.readFileSync(`${__dirname}/testinput.txt`, 'utf8');
+const input = fs.readFileSync(`${__dirname}/input.txt`, "utf8");
 
-const directions = input.trim().split('');
+const directions = input.trim().split("");
 
 function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
 class Rock {
-    points = [];
-    placeInTunnel(lowestPoint) {
-        this.points = this.points.map(point => {
-            return [point[0] + lowestPoint + 3, point[1] + 2]
-        })
+  points = [];
+
+  move(direction) {
+    switch (direction) {
+      case ">":
+        this.points = this.points.map((point) => [point[0], point[1] + 1]);
+        break;
+      case "<":
+        this.points = this.points.map((point) => [point[0], point[1] - 1]);
+        break;
+      case "up":
+        this.points = this.points.map((point) => [point[0] + 1, point[1]]);
+        break;
+      case "down":
+        this.points = this.points.map((point) => [point[0] - 1, point[1]]);
+        break;
     }
-
-    move(direction) {
-        console.log('MOVE SIDEWAYS');
-        let movement = 1;
-
-        if (direction == '<') {
-            movement = -1;
-        }
-
-        if (movement == 1 && this.points[this.points.length - 1][1] == TUNNEL_WIDTH - 1) {
-            console.log('CANT MOVE RIGHT')
-            return;
-        }
-
-        if (movement == -1 && this.points[0][1] == 0) {
-            console.log('CANT MOVE LEFT')
-            return;
-        }
-
-        console.log(this.points);
-        this.points = this.points.map(point => [point[0], point[1] + movement]);
-    }
-
-    fall() {
-        console.log('FALL');
-        this.points = this.points.map(point => [point[0] - 1, point[1]]);
-    }
+  }
 }
 
 class HorizontalRock extends Rock {
-    points = [
-        [0, 0],
-        [0, 1],
-        [0, 2],
-        [0, 3],
-    ]
-    height = 1;
-    width = 4;
+  points = [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [0, 3],
+  ];
+  height = 1;
+  width = 4;
 }
 
 class PlusRock extends Rock {
-    points = [
-        [1, 0],
-        [0, 1],
-        [1, 1],
-        [2, 1],
-        [1, 2],
-    ]
-    height = 3;
-    width = 3;
+  points = [
+    [1, 0],
+    [0, 1],
+    [1, 1],
+    [2, 1],
+    [1, 2],
+  ];
+  height = 3;
+  width = 3;
 }
 
 class LRock extends Rock {
-    points = [
-        [0, 0],
-        [0, 1],
-        [0, 2],
-        [1, 2],
-        [2, 2],
-    ]
-    height = 3;
-    width = 3;
+  points = [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [1, 2],
+    [2, 2],
+  ];
+  height = 3;
+  width = 3;
 }
 
 class VerticalRock extends Rock {
-    points = [
-        [0, 0],
-        [1, 0],
-        [2, 0],
-        [3, 0],
-    ]
-    height = 4;
-    width = 1;
+  points = [
+    [0, 0],
+    [1, 0],
+    [2, 0],
+    [3, 0],
+  ];
+  height = 4;
+  width = 1;
 }
 
 class SquareRock extends Rock {
-    points = [
-        [0, 0],
-        [0, 1],
-        [1, 0],
-        [1, 1],
-    ]
-    height = 1;
-    width = 4;
+  points = [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1],
+  ];
+  height = 1;
+  width = 4;
 }
 
 let currentDirection = 0;
 
 function getDirection() {
-    const dir = directions[currentDirection];
+  const dir = directions[currentDirection];
 
-    currentDirection++;
-    if (currentDirection >= directions.length) {
-        currentDirection = 0;
-    }
+  currentDirection++;
+  if (currentDirection >= directions.length) {
+    currentDirection = 0;
+  }
 
-    return dir;
+  return dir;
 }
 
 function printTunnel(currentRock = null) {
-    const tunnelCopy = JSON.parse(JSON.stringify(TUNNEL));
+  const tunnelCopy = JSON.parse(JSON.stringify(TUNNEL));
 
-    if (currentRock) {
-        for (const point of currentRock.points) {
-            tunnelCopy[point[0]][point[1]] = '@';
-        }
+  if (currentRock) {
+    for (const point of currentRock.points) {
+      tunnelCopy[point[0]][point[1]] = "@";
+    }
+  }
+
+  //   console.log(placedRocks);
+  placedRocks.forEach((placedPoint) => {
+    const coordinates = placedPoint.split("-");
+    tunnelCopy[coordinates[0]][coordinates[1]] = "#"; // 👉️ one, two, three, four
+  });
+
+  for (let i = tunnelCopy.length - 1; i >= 0; i--) {
+    let row = "";
+    for (let j = 0; j < tunnelCopy[i].length; j++) {
+      row += tunnelCopy[i][j];
     }
 
-    for (const placedRock of placedRocks) {
-        for (const point of placedRock.points) {
-            tunnelCopy[point[0]][point[1]] = '#';
-        }
-    }
-
-    for (let i = tunnelCopy.length - 1; i >= 0; i--) {
-        let row = '';
-        for (let j = 0; j < tunnelCopy[i].length; j++) {
-            row += tunnelCopy[i][j];
-        }
-
-        console.log(`|${row}|`);
-    }
-    console.log('+-------+');
-
+    console.log(`|${row}|`);
+  }
+  console.log("+-------+");
 }
 
-// const ROCKS = 2022;
-const ROCKS = 5;
+function detectCollision(rock) {
+  for (const point of rock.points) {
+    if (placedRocks.has(`${point[0]}-${point[1]}`)) {
+      return "rock";
+    }
+
+    if (point[0] < 0) {
+      return "floor";
+    }
+
+    if (point[1] < 0 || point[1] >= 7) {
+      return "wall";
+    }
+  }
+}
+
+function placeInTunnel(rock, baseline) {
+  rock.points = rock.points.map((point) => {
+    return [point[0] + baseline + 4, point[1] + 2];
+  });
+}
+
+function getNewBaseline(newlyPlacedRock, baseline) {
+  for (const point of newlyPlacedRock.points) {
+    BASELINE = Math.max(BASELINE, point[0]);
+  }
+  return BASELINE;
+}
+
+async function print(rock) {
+  console.clear();
+  printTunnel(rock);
+  //   await sleep(100);
+  //   await keypress();
+}
+
+function placeRock(rock) {
+  for (const point of rock.points) {
+    placedRocks.add(`${point[0]}-${point[1]}`);
+  }
+}
+
+const keypress = async () => {
+  process.stdin.setRawMode(true);
+  return new Promise((resolve) =>
+    process.stdin.once("data", () => {
+      process.stdin.setRawMode(false);
+      resolve();
+    })
+  );
+};
+
+const ROCKS = 2022;
+// const ROCKS = 10;
 let rocks = [];
-let placedRocks = [];
+let placedRocks = new Set();
+
 while (rocks.length < ROCKS) {
-    rocks = [...rocks, ...[
-        new HorizontalRock(),
-        new PlusRock(),
-        new LRock(),
-        new VerticalRock(),
-        new SquareRock,
-    ]]
+  rocks = [
+    ...rocks,
+    ...[
+      new HorizontalRock(),
+      new PlusRock(),
+      new LRock(),
+      new VerticalRock(),
+      new SquareRock(),
+    ],
+  ];
 }
 
 rocks.splice(ROCKS, rocks.length - ROCKS);
 
-const LOWEST_POINT = 0;
+let BASELINE = -1;
 const TUNNEL_WIDTH = 7;
-const TUNNEL = Array(10).fill().map(() => Array(TUNNEL_WIDTH).fill('.'))
-
-
-console.log(getDirection());
+const TUNNEL = Array(200000)
+  .fill()
+  .map(() => Array(TUNNEL_WIDTH).fill("."));
 
 (async () => {
-    while (rocks.length > 0) {
-        const rock = rocks[0];
-        collision = false;
+  while (rocks.length > 0) {
+    const rock = rocks[0];
 
-        while (!collision) {
-            rock.placeInTunnel(LOWEST_POINT);
-            // console.clear();
-            printTunnel(rock);
-            await sleep(1000);
-            rock.move(getDirection);
-            // console.clear();
-            printTunnel(rock);
-            await sleep(1000);
-            rock.fall();
-            // console.clear();
-            printTunnel(rock);
-            await sleep(1000);
-        }
+    placeInTunnel(rock, BASELINE);
 
-        rock.placeInTunnel(LOWEST_POINT);
-        // placedRocks.push(rock);
-        rocks = [];
-        rocks.splice(0, 1);
+    // await print(rock);
+    // process.exit(0);
 
+    while (true) {
+      const horizontalDirection = getDirection();
+      rock.move(horizontalDirection);
+
+      let collision = detectCollision(rock);
+      if (collision == "rock" || collision == "wall") {
+        rock.move(horizontalDirection == "<" ? ">" : "<");
+        // placeRock(rock);
+      }
+
+      //   await print(rock);
+
+      rock.move("down");
+      collision = detectCollision(rock);
+      if (collision == "rock" || collision == "floor") {
+        rock.move("up");
+        placeRock(rock);
+        getNewBaseline(rock);
+        break;
+      }
+
+      //   await print(rock);
     }
+
+    // placedRocks.push(rock);
+    rocks.splice(0, 1);
+  }
+
+  console.log("RESULT", BASELINE + 1);
 })();
-
-
-
